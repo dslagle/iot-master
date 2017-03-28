1bbb8695d2e0ab44025b02692b5c083d3356ed91
dslagle <slagle.derek@gmail.com>
2017-03-28 14:58:43 -0400

    added middleware function to properly format client ip address

--- routes.js
+++ routes.js
@@ -19,7 +19,7 @@ router.get("/median/:nums", function(request, response) {
 });
 
 router.post("/register", function(request, response) {
-    console.log(`Register request from ${request.ip}`);
+    console.log(`Register request from ${request.ip.replace(/^:{0,1}ffff:/, '')}`);
     response.json({ success: true });
 });
 
